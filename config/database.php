54590c5c2451a8670b94a3799b9dafdd76aedc02
anamerasul <anamerasul@gmail.com>
2023-09-13 04:22:28 +0600

    test

--- config/database.php
+++ config/database.php
@@ -43,14 +43,36 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
-            'driver'         => 'mysql',
+        // 'mysql' => [
+        //     'driver'         => 'mysql',
+        //     'url'            => env('DATABASE_URL'),
+        //     'host'           => 'localhost', 
+        //     'port'           => 3306,
+        //     'database'       => 'rev.gigple',
+        //     'username'       => 'rev.gigple',
+        //     'password'       => 'rev.gigple',
+        //     'unix_socket'    => '',
+        //     'charset'        => 'utf8mb4',
+        //     'collation'      => 'utf8mb4_unicode_ci',
+        //     'prefix'         => '',
+        //     'prefix_indexes' => true,
+        //     'strict'         => false,
+        //     'engine'         => null,
+        //     // 'engine'         => 'InnoDB ROW_FORMAT=DYNAMIC',
+        //     'options'        => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
+
+
+               'mysql' => [
+            'driver'         => env('DB_CONNECTIONL'),
             'url'            => env('DATABASE_URL'),
-            'host'           => 'localhost',
-            'port'           => 3306,
-            'database'       => 'rev.gigple',
-            'username'       => 'rev.gigple',
-            'password'       => 'rev.gigple',
+            'host'           => env('DB_HOST'), 
+            'port'           => env('DB_PORT'),
+            'database'       =>env('DB_DATABASE'),
+            'username'       =>env('DB_USERNAME'),
+            'password'       => env('DB_PASSWORD'),
             'unix_socket'    => '',
             'charset'        => 'utf8mb4',
             'collation'      => 'utf8mb4_unicode_ci',
@@ -63,6 +85,9 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
+
+
 
         'pgsql' => [
             'driver' => 'pgsql',
