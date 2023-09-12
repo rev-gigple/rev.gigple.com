1062ffd9d64d90715263f5ba9a41dd8d50729961
[anamerasul] <[anamerasul@gmail.com]>
2023-09-12 12:23:20 +0000

    all

--- storage/framework/views/cf8c045b361d9292d0e3f6253fedf059.php
+++ storage/framework/views/cf8c045b361d9292d0e3f6253fedf059.php
@@ -108,7 +108,7 @@
 
 <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> -->
 
-
+<script src="https://cdn.tailwindcss.com"></script>
 
             <div class="let_started_area ">
 <h1 class="text-red">Lets started</h1>
