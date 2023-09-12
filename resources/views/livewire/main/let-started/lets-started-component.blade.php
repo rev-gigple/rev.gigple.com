5f7d58bc6862c7740e971697bdc74f8cd0bf5925
anamerasul <anamerasul@gmail.com>
2023-09-12 18:15:26 +0600

    test

--- resources/views/livewire/main/let-started/lets-started-component.blade.php
+++ resources/views/livewire/main/let-started/lets-started-component.blade.php
@@ -109,48 +109,8 @@
 <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> -->
 
 
-        <main>
-            <div class="let_started_area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12 text-center">
-                            <h2>Lets get Started</h2>
-                            <p>First, tell us what you're looking for.</p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="let_started_left text-end">
-                                <h3 class="mt_25">I'm looking</h3>
-                                <h3><span>for work</span></h3>
-                                <p class="mt_25 mb_25">Find great freelance and part-time <br> work with amazing companies.</p>
-                                <div class="new_button_area d-flex justify-content-end">
-                                    <div class="lets-line"></div>
-                                    <!-- <a href="#" class="lets_button lets_button_two">HIRE</a> -->
 
+            <div class="let_started_area ">
+<h1 class="text-red">Lets started</h1>
 
-                     @guest
-                    <a href="{{ url('auth/register') }}" class="py-2 px-4 text-primary-600 hover:text-primary-800 transition-colors duration-300 rounded-full hidden md:inline-block dark:text-gray-100 dark:hover:text-white">
-                        @lang('messages.t_join')
-                    </a>
-                @endguest
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="let_started_right">
-                                <h3 class="mt_25">I want to hire</h3>
-                                <h3><span>an expert</span></h3>
-                                <p class="mt_25 mb_25">Find and collaborate <br> with a specialist in their field.</p>
-                                <div class="new_button_area">
-                                    <a href="#" class="lets_button">HIRE</a>
-                                    <div class="lets-line"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </main>
-
