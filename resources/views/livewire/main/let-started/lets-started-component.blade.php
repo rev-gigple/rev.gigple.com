01b8b680779e51f17e804449480d3ffba19038bc
anamerasul <anamerasul@gmail.com>
2023-09-12 17:07:03 +0600

    test

--- resources/views/livewire/main/let-started/lets-started-component.blade.php
+++ resources/views/livewire/main/let-started/lets-started-component.blade.php
@@ -1,12 +1,41 @@
 <!-- resources/views/livewire/main/let-started/lets-started-component.blade.php -->
 
-<div>
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        <!-- {{ __('Let\'s Get Started') }} -->
-    </h2>
-    <div class="p-6">
-        <!-- Your content here -->
 
-        <h1>Lets get Started new</h1>
-    </div>
-</div>
+        <main>
+            <div class="let_started_area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <h2>Lets get Started</h2>
+                            <p>First, tell us what you're looking for.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="let_started_left text-end">
+                                <h3 class="mt_25">I'm looking</h3>
+                                <h3><span>for work</span></h3>
+                                <p class="mt_25 mb_25">Find great freelance and part-time <br> work with amazing companies.</p>
+                                <div class="new_button_area d-flex justify-content-end">
+                                    <div class="lets-line"></div>
+                                    <a href="#" class="lets_button lets_button_two">HIRE</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="let_started_right">
+                                <h3 class="mt_25">I want to hire</h3>
+                                <h3><span>an expert</span></h3>
+                                <p class="mt_25 mb_25">Find and collaborate <br> with a specialist in their field.</p>
+                                <div class="new_button_area">
+                                    <a href="#" class="lets_button">HIRE</a>
+                                    <div class="lets-line"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
